c50b8b81b14dbde6ce83f0a00c367a4807e207a5
Nicolas <nicolascamara29@gmail.com>
2018-11-06 18:44:29 -0500

    Parallax2D
    initial commit

---
+++ Parallax2D/Assets/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public float spawnRate = 1f;
+
+    public GameObject hex;
+
+    private float nextTimeToSpawn = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.time >= nextTimeToSpawn)
+        {
+            Instantiate(hex, Vector3.zero, Quaternion.identity);
+            nextTimeToSpawn = Time.time + 1f / spawnRate;
+        }
+		
+	}
+}
